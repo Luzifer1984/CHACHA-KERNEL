330982e2f4da1d481f55b6ef801623b908aa17ec
Kevindeving <98kevchen@gmail.com>
2013-08-19 10:41:00 +0200

    Update smartass2 and smartassh3

--- drivers/cpufreq/cpufreq_smartassH3.c
+++ drivers/cpufreq/cpufreq_smartassH3.c
@@ -70,19 +70,19 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP 80000
+#define DEFAULT_RAMP_DOWN_STEP 90000
 static unsigned int ramp_down_step;
 
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 85
+#define DEFAULT_MAX_CPU_LOAD 75
 static unsigned long max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 70
+#define DEFAULT_MIN_CPU_LOAD 60
 static unsigned long min_cpu_load;
 
 /*
